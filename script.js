57b3cbb4d52c37a027310827d679e30850ba4805
TheMGRF <thomas.grf@googlemail.com>
2020-11-19 14:27:16 +0000

    Add infinite block cycle, typings vscode support and rework and clean up some code

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+"use strict";
+
 const renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement); // Connecting to the canvas
@@ -6,77 +8,56 @@ const scene = new THREE.Scene();
 scene.background = new THREE.Color(0x000000);
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.x = 0;
+camera.position.x = -2;
 camera.position.y = 4;
 camera.position.z = 10;
 
-for (let i = 0; i < 5; i++) {
-    const geo = new THREE.BoxGeometry(1, 1, 1);
-    const mat = new THREE.MeshBasicMaterial({ color: 0xFF0000 });
-    const mesh = new THREE.Mesh(geo, mat);
-    mesh.position.x = 2 * Math.random(3 % i);
-    mesh.position.y = 6 * Math.random();
-    mesh.position.z = -10;
-    
-    scene.add(mesh);
+let cleared = false;
+let meshes = [];
 
-    const light = new THREE.PointLight(0xFF0000, 1, 2000);
-    light.parent = scene;
-    // const textureLoader = new THREE.TextureLoader();
-    // const lensflare = new THREE.LensFlare();
+async function run() {
+    while (true) {
+        if (!cleared) {
+            meshes.forEach(element => {
+                scene.remove(element);
+            });
+            meshes = [];
 
-    // const flare0 = textureLoader.load("lensflare0.png");
-    // const flare1 = textureLoader.load("lensflare1.png");
-    // const flare2 = textureLoader.load("lensflare2.png");
-    // const flare3 = textureLoader.load("lensflare3.png");
-
-    // lensflare.addElement(new LensflareElement(flare0, 512, 0));
-    // lensflare.addElement(new LensflareElement(flare1, 512, 0));
-    // lensflare.addElement(new LensflareElement(flare2, 512, 0));
-    // lensflare.addElement(new LensflareElement(flare3, 60, 0.6));
-
-    // light.add(lensflare);
-
-    light.position.set(50, 50, 50);
-    scene.add(light);
-
+            debug("rawr")
+            for (let i = 0; i < 5; i++) {
+                box(0xFF0000, i);
+            }
+            
+            for (let i = 0; i < 5; i++) {
+                box(0x0000FF, i);
+            }
+        }
+        await sleep(2000);
+    }
 }
 
-for (let i = 0; i < 5; i++) {
+run();
+
+function box(colour, count) {
     const geo = new THREE.BoxGeometry(1, 1, 1);
-    const mat = new THREE.MeshBasicMaterial({ color: 0x0000FF });
+    const mat = new THREE.MeshBasicMaterial({ color: colour });
     const mesh = new THREE.Mesh(geo, mat);
-    mesh.position.x = -6 * Math.random(3 % i);
+    mesh.position.x = -6 * Math.random(3 % count);
     mesh.position.y = 6 * Math.random();
     mesh.position.z = -10;
     
     scene.add(mesh);
-
-    const light = new THREE.PointLight(0xFF0000, 1, 2000);
-    light.parent = scene;
-    // const textureLoader = new THREE.TextureLoader();
-    // const lensflare = new THREE.LensFlare();
-
-    // const flare0 = textureLoader.load("lensflare0.png");
-    // const flare1 = textureLoader.load("lensflare1.png");
-    // const flare2 = textureLoader.load("lensflare2.png");
-    // const flare3 = textureLoader.load("lensflare3.png");
-
-    // lensflare.addElement(new LensflareElement(flare0, 512, 0));
-    // lensflare.addElement(new LensflareElement(flare1, 512, 0));
-    // lensflare.addElement(new LensflareElement(flare2, 512, 0));
-    // lensflare.addElement(new LensflareElement(flare3, 60, 0.6));
-
-    // light.add(lensflare);
-
-    light.position.set(50, 50, 50);
-    scene.add(light);
-
+    meshes.push(mesh);
 }
 
 function animate() {
     requestAnimationFrame(animate);
     camera.translateZ(-0.03);
+
+    if (camera.position.z < -10) {
+        cleared = true;
+        camera.position.z = 10;
+    }
     renderer.render(scene, camera);
 }
 
